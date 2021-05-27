ede5a03ae9e1bc742a09aba89bbd69396bf48b0f
ShAd-x <riValofking@gmail.com>
2021-05-27 10:15:08 +0200

    Code du prof (mais bugué)

--- Casse_Brique/Main.cpp
+++ Casse_Brique/Main.cpp
@@ -1,18 +1,31 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <windows.h>
 
 int main() {
+    sf::RenderWindow window(sf::VideoMode(200, 200), "Casse Brique");
 
-	sf::RenderWindow window(sf::VideoMode(800, 600), "Title");
+    sf::CircleShape cercle(10);
+    cercle.setFillColor(sf::Color::Red);  // Blue Green Black White Cyan Magenta 
+    cercle.setPosition(100, 100);
 
-	sf::Event e;
+    sf::RectangleShape raqette({100, 20});
+    raqette.setFillColor(sf::Color::White);  // Blue Green Black White Cyan Magenta 
 
-	while (window.isOpen()) {
-		while(window.pollEvent(e)){
-			if (e.type == sf::Event::Closed) {
-				window.close();
-			}
-		}
-	}
-	return 0;
+    int posx = 150;
+    int posy = 100;
+
+    while (window.isOpen()) {
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+            posx++;
+        }
+        raqette.setPosition(posx, posy);
+
+        window.draw(cercle);
+        window.draw(raqette);
+        window.display();
+
+        Sleep(30);
+    };
+    return 0;
 }
